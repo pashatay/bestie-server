75bef0b8de2524072d4ac2e820f9a231c4958611
Pasha Tay <pashabonilla@gmail.com>
2019-12-23 23:19:50 -0500

    .

--- src/validate-token.js
+++ src/validate-token.js
@@ -2,7 +2,6 @@ const logger = require("./logger");
 
 function validateBearerToken(req, res, next) {
   const authToken = req.get("Authorization");
-  logger.error(`Unauthorized request to path: ${req.path}`);
   if (!authToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
