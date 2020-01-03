0b563910eb0df22aadf511f504769c773062b0e7
Pasha Tay <pashabonilla@gmail.com>
2020-01-02 23:43:04 -0500

    .

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/bestie",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://postgres@localhost/bestie",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/bestie-test",
   JWT_KEY: process.env.JWT_KEY || "Alberto",
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD
 };
--- test/app.test.js
+++ test/app.test.js
@@ -37,7 +37,7 @@ describe("Endpoints", () => {
   before("make knex instance", () => {
     db = knex({
       client: "pg",
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set("db", db);
   });
